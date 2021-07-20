5590edd6c09a5cdc9401741baed5c5181a3f2f68
Ariel <arieldjal2@gmail.com>
2021-07-20 16:28:34 -0600

    Primer commit proyecto base

---
+++ src/main/java/ac/cr/ucr/examen2ing/Examen2IngApplication.java
@@ -0,0 +1,13 @@
+package ac.cr.ucr.examen2ing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examen2IngApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examen2IngApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ac/cr/ucr/examen2ing/Examen2IngApplicationTests.java
@@ -0,0 +1,13 @@
+package ac.cr.ucr.examen2ing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examen2IngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
